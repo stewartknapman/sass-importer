92e0d24b71a2eb54a6b5ce9013140d056b9f2720
Stewart Knapman <info@stewartknapman.com>
2016-02-04 14:47:19 +1300

    working -> good enough

--- bin/sassimport.js
+++ bin/sassimport.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var concat = require('concat');
 var argv = require('minimist')(process.argv.slice(2));
 var SassImporter = require('../lib/sass-importer.js');
 
-//var paths = new SassImporter();
-
-console.log('argv:', argv);
+if (argv._[0] && argv._[1]) {
+  var paths = new SassImporter(argv._[0]);
+  concat(paths, argv._[1], function (error) {
+    if (error) console.log('ERR:', error);
+  });
+  
+} else {
+  console.log('ERR: Please supply an input and/or output file');
+}
